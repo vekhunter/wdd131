910923b0dee91719f0784d0ff4a259656cdafd63
vekhunter <27ggibson@go.dsdmail.net>
2026-06-11 13:06:53 -0600

    Add week 8 JS objects ponder page

---
+++ ponder/courses.js
@@ -0,0 +1,59 @@
+const aCourse = {
+  code: "CSE121b",
+  name: "Javascript Language",
+  logo: "images/js-logo.png",
+  sections: [
+    {
+      sectionNum: 1,
+      roomNum: "STC 353",
+      enrolled: 26,
+      days: "TTh",
+      instructor: "Bro T"
+    },
+    {
+      sectionNum: 2,
+      roomNum: "STC 347",
+      enrolled: 28,
+      days: "TTh",
+      instructor: "Sis A"
+    }
+  ],
+  enrollStudent: function (sectionNum) {
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNum == sectionNum
+    );
+
+    if (sectionIndex >= 0) {
+      this.sections[sectionIndex].enrolled++;
+      renderSections(this.sections);
+    }
+  }
+};
+
+function setCourseInfo(course) {
+  document.querySelector("#courseName").textContent = course.name;
+  document.querySelector("#courseCode").textContent = course.code;
+}
+
+function sectionTemplate(section) {
+  return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td>
+    </tr>`;
+}
+
+function renderSections(sections) {
+  const html = sections.map(sectionTemplate);
+  document.querySelector("#sections").innerHTML = html.join("");
+}
+
+setCourseInfo(aCourse);
+renderSections(aCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.enrollStudent(sectionNum);
+});
